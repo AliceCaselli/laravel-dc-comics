567f071cbb3a73100d6467c880eb93cdce8e6ec5
AliceCaselli <alicecaselli3@gmail.com>
2023-05-20 17:14:42 +0200

    elenco in pagina

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts/main')
 
 @section('content')
-    
+    <table class="table table-striped">
+        <thead>
+            <th>
+                Titolo
+            </th>
+            <th>
+                Descrizione
+            </th>
+            <th>
+                Prezzo
+            </th>
+            <th>
+                Serie
+            </th>
+            <th>
+                Data di Lancio
+            </th>
+            <th>
+                Tipo
+            </th>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->description}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->type}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
