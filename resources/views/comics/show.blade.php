6cdffa77c3901c01ecc673ed10e9fa1fd323fec9
AliceCaselli <alicecaselli3@gmail.com>
2023-05-22 03:16:36 +0200

    fix: route in form delete

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -45,7 +45,7 @@
 
         <div class="d-flex justify-content-center">
             <a class="btn btn-primary" href="{{route('comics.edit', $comic)}}">Modifica fumetto</a>
-            <form action="{{route('comics/destroy', $comic->id)}}" method="POST">
+            <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="btn btn-danger mx-5">Elimina fumetto</button>
